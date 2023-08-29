fc640190717ef40aa22778598ebb71aca483d34e
rahuldivyansh <90146156+rahuldivyansh@users.noreply.github.com>
2023-08-29 23:33:21 +0530

    Added basic navbar layout

---
+++ src/components/sections/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <div>
+          <div className="start">start </div>
+          <div className="step">step</div>
+          <div className="end">end</div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
