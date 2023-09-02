2c16526c8dce7273138678d99314bb3b10334e6a
Rahultadmybrand <rahul.thakur@admybrand.com>
2023-09-02 20:57:48 +0530

    init

--- pages/movie/[movie_id].jsx
+++ pages/movie/[movie_id].jsx
@@ -32,7 +32,7 @@ export const getServerSideProps = async (context) => {
       },
     };
   } catch (error) {
-    // console.log(error);
+    console.log(error);
     return {
       notFound: true,
     };
